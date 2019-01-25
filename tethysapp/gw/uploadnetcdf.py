d2bee969c57f4dd1dd675ff3a44e5bada9186d97
stevenwevans2 <stevenwevans2@gmail.com>
2019-01-25 11:50:14 -0700

    minor fix

--- tethysapp/gw/uploadnetcdf.py
+++ tethysapp/gw/uploadnetcdf.py
@@ -22,8 +22,8 @@ from .app import Gw as app
 
 porosity=0.3
 #global variables
-thredds_serverpath='/opt/tomcat/thredds/public/testdata/groundwater/'
-#thredds_serverpath = "/home/student/tds/apache-tomcat-8.5.30/content/thredds/public/testdata/groundwater/"
+thredds_serverpath='/opt/tomcat/content/thredds/public/testdata/groundwater/'
+# thredds_serverpath = "/home/student/tds/apache-tomcat-8.5.30/content/thredds/public/testdata/groundwater/"
 
 #This function opens the Aquifers.csv file for the specified region and returns a JSON object listing the aquifers
 def getaquiferlist(app_workspace,region):
